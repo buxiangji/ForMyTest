cfe66e0d7088a94d95df564cc531ef16602d2a3a
buxiangji <1763851823@qq.com>
2020-08-12 15:13:41 +0800

    新建测试项目

---
+++ forMyTest/src/main/java/com/boxj/Application.java
@@ -0,0 +1,16 @@
+package com.boxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author
+ * @description
+ * @create: 2020-08-12 15:00
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
